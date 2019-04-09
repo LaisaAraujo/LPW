d62dfc9a7f549e2177b416dd15f4408cbda8e4bb
Luann Campos <luann-ac@outlook.com>
2019-04-09 11:15:55 -0300

    Popoper

---
+++ FJI Corp/Models/Agil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FJI_Corp.Models
+{
+    public class Agil
+    {
+        public void cardapio(String nome)
+        {
+
+        }
+    }
+}
